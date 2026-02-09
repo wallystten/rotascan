546f972094c321f98469c535124c7b3ffb1fba52
wallystten <alfredosilvabarbosa29@gmail.com>
2026-02-09 18:10:21 -0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,19 @@
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("rotascan").then(cache => {
+      return cache.addAll([
+        "./",
+        "index.html",
+        "style.css",
+        "script.js",
+        "manifest.json"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
